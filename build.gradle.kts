054c952d1f35fcc17c1d25893389a439817f54d1
muradhani <mouradhany574@gmail.com>
2025-04-11 16:08:04 +0200

    ``` feat: Add network dependencies and INTERNET permission
    This commit introduces network-related dependencies and the INTERNET permission to the project.
    
    - Added Ktor client dependencies (core, serialization, logging, android) to the `:network` module's `build.gradle.kts`.
    - Added Kotlin serialization dependency to the `:network` module's `build.gradle.kts`.
    - Updated `libs.versions.toml` to include versions for Ktor and serialization.
    - Added INTERNET permission to `AndroidManifest.xml` of the `:network` module.
    ```

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,4 +40,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.serilization)
+    implementation(libs.ktor.client.logging)
+    implementation(libs.ktor.client.android)
+    implementation(libs.kotlin.serilization)
 }
