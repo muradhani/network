6bbb96b5baef53f0312f2257f31643f77c74559f
muradhani <mouradhany574@gmail.com>
2025-05-01 22:45:32 +0300

    feat: Implement character fetching in KtorClient

--- src/main/java/com/example/network/KtorClient.kt
+++ src/main/java/com/example/network/KtorClient.kt
@@ -1,5 +1,6 @@
 package com.example.network
 
+import com.example.network.dto.CharacterDto
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.android.Android
@@ -7,16 +8,16 @@ import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.logging.SIMPLE
-
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.cio.Response
 import io.ktor.serialization.kotlinx.json.json
-import kotlinx.serialization.Serializable
-
 import kotlinx.serialization.json.Json
+import javax.inject.Inject
 
 
-class KtorClient {
+class KtorClient @Inject constructor() {
     private val client = HttpClient(Android){
         defaultRequest { url("https://rickandmortyapi.com/api/") }
         install(Logging){ logger = Logger.SIMPLE }
@@ -26,19 +27,12 @@ class KtorClient {
             })
         }
     }
-//    suspend fun getCharacter(id:Int): CharacterDto {
-//        return client.get("character/$id").body()
-//    }
-}
-
-@Serializable
-data class Character(
-    val id : Int,
-    val name : String,
-    val origin :Origin
-){
-    @Serializable
-    data class Origin(
-        val name: String
-    )
+    suspend fun getCharacter(id: Int): CharacterDto? {
+        val response = client.get("character/$id")
+        return if (response.status == HttpStatusCode.OK) {
+            response.body<CharacterDto>() // Convert response body to CharacterDto
+        } else {
+            null // Handle error case
+        }
+    }
 }
