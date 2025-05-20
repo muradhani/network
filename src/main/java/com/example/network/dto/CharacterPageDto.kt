3b92e2949d6100f16ccf0e2337ec5f015a3eba00
murad.hani <murad.hani@vodafone.com>
2025-05-20 11:04:02 +0300

    Add @Serializable annotation to CharacterPageDto and InfoDto
    This commit marks `CharacterPageDto` and `InfoDto` data classes with the `@Serializable` annotation, enabling them to be processed by Kotlinx Serialization.

--- src/main/java/com/example/network/dto/CharacterPageDto.kt
+++ src/main/java/com/example/network/dto/CharacterPageDto.kt
@@ -1,9 +1,13 @@
 package com.example.network.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class CharacterPageDto(
     val info: InfoDto,
     val results: List<CharacterDto>
 )
+@Serializable
 data class InfoDto(
     val count: Int,
     val pages: Int,
